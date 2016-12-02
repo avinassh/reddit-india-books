4dcba7be97d5b1266166994c29d7316f7f0d00a8
avi <hi@avi.im>
2016-12-02 12:44:01 +0530

    Update main script and add logging

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import shutil
 import time
 import json
+import logging
+import logging.handlers
 
 import requests
 from jinja2 import Environment, FileSystemLoader
@@ -15,6 +17,22 @@ SOURCE_FILE = os.path.join(BASE_DIR, 'source.txt')
 TEMPLATE_FILE = 'template.html'
 TEMPLATE_ENVIRONMENT = Environment(
     autoescape=False, loader=FileSystemLoader(BASE_DIR), trim_blocks=False)
+# Logging settings
+LOG_DIR = os.path.join(BASE_DIR, 'logs')
+LOG_FILENAME = os.path.join(LOG_DIR, 'output_log')
+my_logger = logging.getLogger('MyLogger')
+
+
+def set_logger():
+    my_logger = logging.getLogger('MyLogger')
+    my_logger.setLevel(logging.DEBUG)
+    handler = logging.handlers.RotatingFileHandler(LOG_FILENAME,
+                                                   maxBytes=20000000,
+                                                   backupCount=50,
+                                                   )
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    handler.setFormatter(formatter)
+    my_logger.addHandler(handler)
 
 
 def create_images_dir():
@@ -37,6 +55,7 @@ def download_image(image_url, image_name):
 
 
 def get_book_data(source_file=SOURCE_FILE):
+    my_logger.debug('Reading from source file')
     books = []
     with open(source_file) as f:
         for line in f.readlines():
@@ -49,27 +68,33 @@ def get_book_data(source_file=SOURCE_FILE):
                 'suggested_by': suggested_by
             }
             books.append(book)
+    my_logger.debug('Reading from source file complete')
+    my_logger.debug("Total books: {}".format(len(books)))
     return books
 
 
 def get_goodreads_data(books):
-    books = books[:10]
     for book in books:
         search_term = "{} {}".format(book['name'], book['author'])
+        my_logger.debug("Fetching book details for: {}".format(search_term))
         try:
             book_goodreads = get_book_details_by_name(book_name=search_term)
         except BookNotFound:
+            my_logger.debug("Book not found: {}".format(search_term))
             continue
         keys = ['image_url', 'publication_year', 'num_pages', 'average_rating',
                 'ratings_count', 'url', 'goodreads_id']
         for key in keys:
             book[key] = book_goodreads[key]
         time.sleep(1)
+    my_logger.debug('Fetching book details from Goodreads is complete')
+    my_logger.debug("Total books fetched: {}".format(len(books)))
     return books
 
 
 def download_book_covers(books):
     for book in books:
+        my_logger.debug("Downloading book cover for: ".format(book['name']))
         image_url = book['image_url']
         extention = image_url.split('.')[-1]
         image_name = "{}.{}".format(book['goodreads_id'], extention)
@@ -84,10 +109,13 @@ def render_template(template_filename, context):
 
 
 def write_output(books):
+    my_logger.debug('Generating output')
     output = os.path.join(DIST_DIR, 'index.html')
     html = render_template(TEMPLATE_FILE, context={'books': books})
     with open(output, 'w') as f:
         f.write(html)
+    my_logger.debug('Output generation done!')
+    my_logger.debug('Woooooot!')
 
 
 def main():
@@ -101,5 +129,8 @@ def main():
 
 
 if __name__ == '__main__':
+    set_logger()
+    my_logger.debug('----- starting the script -----')
     create_images_dir()
     main()
+    my_logger.debug('----- bye, thanks for all the fish -----')
